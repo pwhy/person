bb04a0f3eec64f8d2917b772c13b5fc87185465b
Py LOPEZ <py.lopez@free.fr>
2012-04-06 14:21:48 +0200

    modifs

--- person.rb
+++ person.rb
@@ -1,27 +1,9 @@
 class Person
 
+	attr_accessor :nom, :prenom, :age
+
 	def initialize(un_nom, un_prenom)
 		@nom = un_nom
-		@prenom = un_prenom
-	end
-	
-	# getter
-	def nom
-		return @nom
-	end
-	
-	# setter
-	def nom=(un_nom)
-		@nom = un_nom
-	end
-
-	#getter
-	def prenom
-		return @prenom
-	end
-	
-	#setter
-	def prenom=(un_prenom)
 		@prenom = un_prenom
 	end
 	
